072d83c4d7302b1f47eb51bd5a93de24b723c0a6
UGUP <upma121gupta@gmail.com>
2020-01-06 00:02:14 +0530

    [USER/QUESTION] Comments added to the functionlities
    Signed-off-by: UGUP <upma121gupta@gmail.com>

--- quora-api/src/main/java/com/upgrad/quora/api/controller/UserController.java
+++ quora-api/src/main/java/com/upgrad/quora/api/controller/UserController.java
@@ -1,17 +1,24 @@
 package com.upgrad.quora.api.controller;
 
-import com.upgrad.quora.api.model.*;
+import com.upgrad.quora.api.model.SigninResponse;
+import com.upgrad.quora.api.model.SignoutResponse;
+import com.upgrad.quora.api.model.SignupUserRequest;
+import com.upgrad.quora.api.model.SignupUserResponse;
 import com.upgrad.quora.service.business.UserBusinessService;
-import com.upgrad.quora.service.entity.QuestionEntity;
 import com.upgrad.quora.service.entity.UserAuthEntity;
 import com.upgrad.quora.service.entity.UserEntity;
-import com.upgrad.quora.service.exception.*;
+import com.upgrad.quora.service.exception.AuthenticationFailedException;
+import com.upgrad.quora.service.exception.SignOutRestrictedException;
+import com.upgrad.quora.service.exception.SignUpRestrictedException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Base64;
 import java.util.UUID;
@@ -23,6 +30,10 @@ public class UserController {
     @Autowired
     private UserBusinessService userBusinessService;
 
+    /*The Sign up Api end point helps the user to register in the application
+     *This is a post request called with "/user/signup.
+     *It collects all the data from the user and posts in database after validation
+     */
     @RequestMapping(method = RequestMethod.POST, path = "/user/signup", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public ResponseEntity<SignupUserResponse> signup(final SignupUserRequest signupUserRequest) throws SignUpRestrictedException {
 
@@ -46,6 +57,11 @@ public class UserController {
 
     }
 
+    /*This Api end point is a post request which takes in the user and password in base 64 format from the request
+     *It decodes the base 64 format and separate out the username and password
+     * After decoding the base 64 format it sends the username and password for validation
+     *After successful validation the user is logged in the application
+     */
     @RequestMapping(method = RequestMethod.POST, path = "/user/signin", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public ResponseEntity<SigninResponse> signin(@RequestHeader("authorization") final String authorization) throws AuthenticationFailedException {
         byte[] decode = Base64.getDecoder().decode(authorization.split("Basic ")[1]);
@@ -59,16 +75,16 @@ public class UserController {
     }
 
 
-
+    /*This api endpoint takes in the post request and is responsible for successful signout of User.
+     *It takes in the parameter of access token generated at the time of login
+     * Signs out if the user is logged in and has not signed out yet.
+     */
     @RequestMapping(method = RequestMethod.POST, path = "/user/signout", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public ResponseEntity<SignoutResponse> signout(@RequestHeader("authorization") String accessToken) throws SignOutRestrictedException {
         UserAuthEntity userAuthEntity = userBusinessService.accessTokenValidation(accessToken);
         return new ResponseEntity<SignoutResponse>(new SignoutResponse().id(userAuthEntity.getUuid()).message("SIGNED OUT SUCCESSFULLY"), HttpStatus.OK);
 
     }
-
-
-
 
 
 }
